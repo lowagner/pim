ce709ac3fb7cbe1af380ecfbc9a7cb4b0cd1c3ea
Lucas Wagner <lowagner@gmail.com>
2023-10-14 18:57:54 -0700

    add initial Script parser skeleton

--- src/parser.rs
+++ src/parser.rs
@@ -6,12 +6,51 @@ use directories as dirs;
 use crate::brush::BrushMode;
 use crate::gfx::Rgba8;
 use crate::platform;
+use crate::script::{Argument, Command, Script};
 use crate::session::{Direction, Mode, VisualState};
 
 use std::ffi::OsString;
 use std::str::FromStr;
 
 pub type Error = memoir::result::Error;
+
+impl Parse for Script {
+    fn parser() -> Parser<Self> {
+        get_script_parser(-1)
+    }
+}
+
+fn get_script_parser(lookback: i32) -> Parser<Script> {
+    assert!(
+        lookback < 0,
+        "lookback should start at -1 and go more negative"
+    );
+    // It's safe to unwrap this token here since token is non-whitespace
+    // and the only command parse error is if the string is just whitespace.
+    let command = token().map(|s| Command::from_str(&s).unwrap());
+
+    command
+        .skip(whitespace())
+        .then(any::<Argument, Vec<Argument>>(get_argument_parser(
+            lookback,
+        )))
+        .map(|(command, arguments)| Script { command, arguments })
+        .label("<script>")
+}
+
+fn get_argument_parser(lookback: i32) -> Parser<Argument> {
+    // TODO: this will explode the stack since each of these parsers call the other.
+    //      we need a way to call lazily.  or implement Script::parser directly.
+    // let script_arg = between('(', ')', get_script_parser(lookback - 1)).map(Argument::Script).label("<nested-script>");
+    let string_arg = quoted().map(Argument::String).label("<string>");
+    let color_arg = color().map(Argument::Color).label("<color>");
+    let i64_arg = integer::<i64>().map(Argument::I64).label("<i64>");
+    // TODO: evaluate any random string as a no-arg Script, besides comments
+
+    peek(string_arg.or(color_arg).or(i64_arg))
+        .skip(optional(whitespace()).skip(optional(comment())))
+        .label("<argument>")
+}
 
 pub fn identifier() -> Parser<String> {
     many::<_, String>(satisfy(
@@ -278,6 +317,14 @@ impl Parse for Mode {
 }
 
 pub fn quoted() -> Parser<String> {
+    single_quoted().or(double_quoted())
+}
+
+pub fn single_quoted() -> Parser<String> {
+    between('\'', '\'', until(symbol('\'')))
+}
+
+pub fn double_quoted() -> Parser<String> {
     between('"', '"', until(symbol('"')))
 }
 
@@ -317,6 +364,46 @@ mod test {
         assert_eq!(a, Rgba8::new(0xff, 0xaa, 0x44, 127));
         assert_eq!(b, Rgba8::new(0x14, 0x14, 0x14, 255));
     }
+
+    #[test]
+    fn test_quotes() {
+        let q = quoted();
+
+        let (result, rest) = q.parse("\"hello world!\"").unwrap();
+        assert_eq!(rest, "");
+        assert_eq!(result, "hello world!".to_string());
+
+        let (result, rest) = q.parse("'cast of cats'").unwrap();
+        assert_eq!(rest, "");
+        assert_eq!(result, "cast of cats".to_string());
+    }
+
+    #[test]
+    fn test_script_multiple_simple_args() {
+        let p = Script::parser();
+
+        let (result, rest) = p.parse("if -3024 'hello' #123456 \"world\"").unwrap();
+        assert_eq!(rest, "");
+        assert_eq!(
+            result,
+            Script {
+                command: Command::If,
+                arguments: vec![
+                    Argument::I64(-3024),
+                    Argument::String("hello".to_string()),
+                    Argument::Color(Rgba8 {
+                        r: 0x12,
+                        g: 0x34,
+                        b: 0x56,
+                        a: 0xff
+                    }),
+                    Argument::String("world".to_string()),
+                ],
+            }
+        );
+    }
+
+    // TODO: test script with comments
 
     #[test]
     fn test_modifiers_parser_success() {
